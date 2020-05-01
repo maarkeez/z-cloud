dfb8f28794ae68fc2e33c0392196eab02b832a86
maarkeez <d.marquez.delgado.91@gmail.com>
2020-05-01 22:03:35 +0200

    Added image to gallery

---
+++ z-cloud/ui/photogallery/PhotoGalleryCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoGalleryCell.swift
+//  z-cloud
+//
+//  Created by David Márquez Delgado on 01/05/2020.
+//  Copyright © 2020 David Márquez Delgado. All rights reserved.
+//
+
+import UIKit
+
+class PhotoGalleryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- z-cloud/ui/photogallery/PhotoGalleryViewController.swift
+++ z-cloud/ui/photogallery/PhotoGalleryViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class PhotoGalleryViewController: UIViewController {
-
+    
     // MARK: - Properties
     private let reuseIdentifier = "PhotoGalleryCell"
+    private let itemsPerRow: CGFloat = 3
+    private let sectionInsets = UIEdgeInsets(top: 2.5, left: 10.0, bottom: 2.5, right: 10.0)
     
     // MARK: - Visual elements
     @IBOutlet weak var myCollectionView: UICollectionView!
@@ -21,21 +23,23 @@ class PhotoGalleryViewController: UIViewController {
         myCollectionView.backgroundColor = UIConfiguration.singleton().color.mainBackground
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
+// MARK: - Extension to handle photo cells
 extension PhotoGalleryViewController: UICollectionViewDataSource{
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -43,20 +47,48 @@ extension PhotoGalleryViewController: UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 50
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView
-          .dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoGalleryCell
         cell.backgroundColor = .black
-        // Configure the cell
+        cell.myImage.contentMode = .scaleAspectFill
+        
         return cell
     }
-    
+        
     
 }
 
+// MARK: - Extension to handle cell events
 extension PhotoGalleryViewController: UICollectionViewDelegate{
+    
+}
+
+// MARK: - Extension to handle layout
+extension PhotoGalleryViewController: UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+//        print("View frame width: \(view.frame.width)")
+//        print("Padding space: \(paddingSpace)")
+//        print("Aviable width: \(availableWidth)")
+//        print("Width per item: \(widthPerItem)")
+//        print("")
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
     
 }
