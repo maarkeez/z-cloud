7c77f061a20b5365971b8a4dea4af70c5d5bc79d
maarkeez <d.marquez.delgado.91@gmail.com>
2020-05-02 13:03:32 +0200

    Settings: Added save button

--- z-cloud/ui/photo/settings/SettingsViewController.swift
+++ z-cloud/ui/photo/settings/SettingsViewController.swift
@@ -13,11 +13,19 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var myMainView: UIView!
     @IBOutlet weak var myHostLbl: UILabel!
     @IBOutlet weak var myPortLbl: UILabel!
+    @IBOutlet weak var myHostText: UITextField!
+    @IBOutlet weak var myPortText: UITextField!
+    @IBOutlet weak var mySaveBtn: UIButton!
+    
+    @IBAction func onSaveClick(_ sender: UIButton) {
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         myMainView.backgroundColor = UIConfiguration.singleton().color.mainBackground
         myHostLbl.textColor = UIConfiguration.singleton().color.pinkBarButtonItem
         myPortLbl.textColor = UIConfiguration.singleton().color.pinkBarButtonItem
+        mySaveBtn.backgroundColor = UIConfiguration.singleton().color.pinkBarButtonItem
+        mySaveBtn.tintColor = .white
     }
 
 }
